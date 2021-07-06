d9ea858104efdea00d38527275117f05b5c345c2
Nguyenthai-109 <86713376+Nguyenthai-109@users.noreply.github.com>
2021-07-06 20:55:08 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,17 @@
-const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+const  CONFIG = {
+    introTitle : 'Heluu cậu !!!' ,
+    introDesc : Tớ có điều này muốn hỏi cậu phải trả lời thật lòng nhaaa ,
+    btnIntro : 'Okk' ,
+    title : 'có chơi với Thái không nào 😙' ,
+    desc : 'Nếu không trả lời sẽ bị ăn đấm' ,
+    btnCó : 'Đương nhiên rồi' ,
+    btnNo : 'mơ à !!' ,
+    question : 'Tại sao lại chơi với Thái ?' ,
+    btnReply : 'Gửi cho tớ :33' ,
+    reply : 'bởi vì cậu đẹp traiii :>>>' ,
+    lộn xộn : 'Tớ biết mà 🥰 hehehee' ,
+    messDesc : 'Ngày mai tớ bao cậu ăn nhé =)))' ,
+    btnAccept : 'okiii lunn <3' ,
+    messlink : 'http://fb.com' ,
 }
+    
