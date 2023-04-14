ad79765ce52b140d93a963a9fdb2ba45a5ffde1b
Nguyenthai-109 <86713376+Nguyenthai-109@users.noreply.github.com>
2023-04-14 09:50:17 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    reply: 'tại hà ngu vãi lon',
     mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Mai anh qua đón nhé công chúa.',
+    messDesc: 'Mai đéo đón hà nhé.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'https://www.facebook.com/profile.php?id=100079743185119'
 }
